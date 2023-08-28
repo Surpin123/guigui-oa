c074027bb11e4e70885e74d9a14ed9008119719a
Surpin123 <18239476430@139.com>
2023-08-28 22:45:51 +0800

    20230828_云尚办公系统_整合knife4j

--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -4,6 +4,8 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.service.SysRoleService;
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -25,6 +28,7 @@ public class SysRoleController {
         return list;
     }*/
 
+    @ApiOperation("查询所有的用户")
     @GetMapping("/findAll")
     public Result findAll() {
         List<SysRole> list = sysRoleService.list();
